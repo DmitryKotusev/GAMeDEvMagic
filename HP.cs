c3b2dc23a5e831664c2722cc45c6da09d9aa1490
Ma-Sk <fil789.gmin@gmail.com>
2017-05-01 22:42:40 +0300

    Add files via upload

---
+++ HP.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HP : Artefact
+{
+    enum HPSize { sml, med, big };
+    public bool isRsb = false;
+    /// <summary>
+    /// Метод восствавливающий здоровье обьекту класса Person
+    /// </summary>
+    /// <returns></returns>
+    public void AddHlt(Person trgPrs,  HPSize size)
+    {
+        if (amount <= 0)
+            Console.WriteLine("У вас нет бутылки живой воды");
+        {
+            if (trgPrs.mana != trgPrs.max_mana)
+            {
+                int ahlt = 0;
+                if (size == sml)
+                    ahlt = 10;
+                if (size == med)
+                    ahlt = 25;
+                if (size == big)
+                    ahlt = 50;
+                if (trgPrs.health + ahlt > trgPrs.max_health)
+                {
+                    ahlt = trgPrs.max_health - trgPrs.health;
+                }
+                trgPrs.health += ahlt;
+                amount--;
+            }
+            else
+                Console.WriteLine("Здоровье персонажа полно");
+        }
+    }
+
+}
---
+++ MP.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HP : Artefact
+{
+    enum MPSize { sml, med, big };
+    public bool isRsb = false;
+    /// <summary>
+    /// Метод восствавливающий ману обьекту класса Person
+    /// </summary>
+    /// <returns></returns>
+    public void AddMana(Person trgPrs,  MPSize size)
+    {
+        if (amount <= 0)
+            Console.WriteLine("У вас нет бутылки мертвой воды");
+        {
+            if (trgPrs.health != trgPrs.max_health)
+            {
+                int aMana = 0;
+                if (size == sml)
+                    aMana = 10;
+                if (size == med)
+                    aMana = 25;
+                if (size == big)
+                    aMana = 50;
+                if (trgPrs.mana + aMana > trgPrs.max_mana)
+                {
+                    aMana = trgPrs.max_mana - trgPrs.mana;
+                }
+                trgPrs.mana += aMana;
+                amount--;
+            }
+            else
+                Console.WriteLine("Мана персонажа полна");
+        }
+    }
+}
